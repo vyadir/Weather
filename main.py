b523554dc6f30fc0654be238340ea63414fd438d
yvega <yadir.sve@gmail.com>
2024-01-04 16:01:20 -0600

    mejoras sintaxis

--- main.py
+++ main.py
@@ -1,24 +1,91 @@
+# Importación de bibliotecas y módulos necesarios
 import os
 from twilio.rest import Client
-from config import TWILIO_ACCOUNT_SID,TWILIO_AUTH_TOKEN,PHONE_NUMBER,API_KEY_WAPI,TO
-import time
+from config import TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, PHONE_NUMBER, API_KEY_WAPI, TO
 from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
 import pandas as pd
-import requests
 from tqdm import tqdm
 from datetime import datetime
-from utils import request_wapi,get_forecast,create_df,send_message,get_date
+from utils import request_wapi, get_forecast, create_df, send_message, get_date
 
-query = 'San Jose, Costa Rica'
-api_key = API_KEY_WAPI
-input_date= get_date()
-response = request_wapi(api_key,query)
-datos = []
-for i in tqdm(range(24),colour = 'green'):
-    datos.append(get_forecast(response,i))
-df_rain = create_df(datos)
-# Send Message
-message_id = send_message(TWILIO_ACCOUNT_SID,TWILIO_AUTH_TOKEN,input_date,df_rain,query,TO)
-print('Mensaje Enviado con exito ' + message_id)
+# Definición de la clase WeatherApp que encapsula la funcionalidad de la aplicación del clima
+class WeatherApp:
+    def __init__(self, api_key, query, to_phone):
+        """
+        Inicializa la instancia de WeatherApp con la clave de la API, la consulta de ubicación y el número de teléfono del destinatario.
+        Args:
+        - api_key (str): Clave de la API para acceder a los datos meteorológicos.
+        - query (str): Ubicación para la consulta meteorológica.
+        - to_phone (str): Número de teléfono del destinatario para enviar mensajes.
+        """
+        self.api_key = api_key
+        self.query = query
+        self.to_phone = to_phone
+
+    def run(self):
+        """
+        Método principal que ejecuta la aplicación del clima.
+        """
+        # Obtiene la respuesta de la consulta meteorológica
+        response = self.request_weather_data()
+        # Obtiene los datos de pronóstico meteorológico
+        datos = self.get_forecast_data(response)
+        # Crea un DataFrame con los datos de pronóstico meteorológico
+        df_rain = self.create_dataframe(datos)
+        # Envía un mensaje con los datos meteorológicos
+        message_id = self.send_weather_message(df_rain)
+        print('Mensaje Enviado con exito ' + message_id)
+
+    def request_weather_data(self):
+        """
+        Realiza una solicitud para obtener datos meteorológicos y devuelve la respuesta.
+        Returns:
+        - response (object): Objeto de respuesta de la solicitud meteorológica.
+        """
+        return request_wapi(self.api_key, self.query)
+
+    def get_forecast_data(self, response):
+        """
+        Obtiene los datos de pronóstico meteorológico para las próximas 24 horas.
+        Args:
+        - response (object): Objeto de respuesta de la solicitud meteorológica.
+        Returns:
+        - datos (list): Lista de datos de pronóstico meteorológico.
+        """
+        datos = []
+        for i in tqdm(range(24), colour='green'):
+            datos.append(get_forecast(response, i))
+        return datos
+
+    def create_dataframe(self, datos):
+        """
+        Crea un DataFrame a partir de los datos de pronóstico meteorológico.
+        Args:
+        - datos (list): Lista de datos de pronóstico meteorológico.
+        Returns:
+        - df_rain (DataFrame): DataFrame creado a partir de los datos.
+        """
+        return create_df(datos)
+
+    def send_weather_message(self, df_rain):
+        """
+        Envía un mensaje con los datos meteorológicos al destinatario.
+        Args:
+        - df_rain (DataFrame): DataFrame con datos meteorológicos.
+        Returns:
+        - message_id (str): ID del mensaje enviado.
+        """
+        input_date = get_date()
+        return send_message(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, input_date, df_rain, self.query, self.to_phone)
+
+# Bloque principal que se ejecuta solo cuando el script se ejecuta directamente
+if __name__ == "__main__":
+    # Configuración de parámetros para la aplicación del clima
+    query_location = 'San Jose, Costa Rica'
+    api_key_wapi = API_KEY_WAPI
+    recipient_phone_number = TO
+    # Creación de una instancia de la clase WeatherApp y ejecución de la aplicación
+    weather_app = WeatherApp(api_key_wapi, query_location, recipient_phone_number)
+    weather_app.run()
--- utils.py
+++ utils.py
@@ -1,51 +1,96 @@
-
+# Importación de bibliotecas y módulos necesarios
 import pandas as pd
 from twilio.rest import Client
-from config import TWILIO_ACCOUNT_SID,TWILIO_AUTH_TOKEN,PHONE_NUMBER,API_KEY_WAPI
+from config import TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, PHONE_NUMBER, API_KEY_WAPI
 from datetime import datetime
 import requests
 from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
 
+# Función para obtener la fecha actual en el formato "YYYY-MM-DD"
 def get_date():
+    """
+    Obtiene la fecha actual en el formato "YYYY-MM-DD".
+    Returns:
+    - input_date (str): Fecha actual en el formato "YYYY-MM-DD".
+    """
     input_date = datetime.now()
     input_date = input_date.strftime("%Y-%m-%d")
     return input_date
 
-def request_wapi(api_key,query):
+# Función para realizar la solicitud a la API del clima y obtener la respuesta
+def request_wapi(api_key, query):
+    """
+    Realiza una solicitud a la API del clima y devuelve la respuesta.
+    Args:
+    - api_key (str): Clave de la API para acceder a los datos meteorológicos.
+    - query (str): Ubicación para la consulta meteorológica.
+    Returns:
+    - response (object): Objeto de respuesta de la solicitud meteorológica.
+    """
     url_clima = 'http://api.weatherapi.com/v1/forecast.json?key='+api_key+'&q='+query+'&days=1&aqi=no&alerts=no'
-    try :
+    try:
         response = requests.get(url_clima).json()
     except Exception as e:
         print(e)
     return response
 
-def get_forecast(response,i):
+# Función para obtener datos específicos del pronóstico meteorológico
+def get_forecast(response, i):
+    """
+    Obtiene datos específicos del pronóstico meteorológico para una hora específica.
+    Args:
+    - response (object): Objeto de respuesta de la solicitud meteorológica.
+    - i (int): Índice de la hora en el pronóstico.
+    Returns:
+    - Tuple: Información de fecha, hora, condición, temperatura, si lloverá, probabilidad de lluvia.
+    """
     fecha = response['forecast']['forecastday'][0]['hour'][i]['time'].split()[0]
     hora = int(response['forecast']['forecastday'][0]['hour'][i]['time'].split()[1].split(':')[0])
     condicion = response['forecast']['forecastday'][0]['hour'][i]['condition']['text']
     tempe = response['forecast']['forecastday'][0]['hour'][i]['temp_c']
     rain = response['forecast']['forecastday'][0]['hour'][i]['will_it_rain']
     prob_rain = response['forecast']['forecastday'][0]['hour'][i]['chance_of_rain']
-    return fecha,hora,condicion,tempe,rain,prob_rain
+    return fecha, hora, condicion, tempe, rain, prob_rain
 
+# Función para crear un DataFrame a partir de los datos del pronóstico meteorológico
 def create_df(data):
-    col = ['Fecha','Hora','Condicion','Temperatura','Lluvia','prob_lluvia']
-    df = pd.DataFrame(data,columns=col)
-    df = df.sort_values(by = 'Hora',ascending = True)
-    df_rain = df[(df['Lluvia']==1) & (df['Hora']>6) & (df['Hora']< 22)]
-    df_rain = df_rain[['Hora','Condicion']]
-    df_rain.set_index('Hora', inplace = True)
+    """
+    Crea un DataFrame a partir de los datos del pronóstico meteorológico.
+    Args:
+    - data (list): Lista de datos del pronóstico meteorológico.
+    Returns:
+    - df_rain (DataFrame): DataFrame creado a partir de los datos.
+    """
+    col = ['Fecha', 'Hora', 'Condicion', 'Temperatura', 'Lluvia', 'prob_lluvia']
+    df = pd.DataFrame(data, columns=col)
+    df = df.sort_values(by='Hora', ascending=True)
+    df_rain = df[(df['Lluvia'] == 1) & (df['Hora'] > 6) & (df['Hora'] < 22)]
+    df_rain = df_rain[['Hora', 'Condicion']]
+    df_rain.set_index('Hora', inplace=True)
     return df_rain
 
-def send_message(TWILIO_ACCOUNT_SID,TWILIO_AUTH_TOKEN,input_date,df,query,TO):
+# Función para enviar un mensaje a través de Twilio con los datos meteorológicos
+def send_message(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, input_date, df, query, TO):
+    """
+    Envía un mensaje a través de Twilio con los datos meteorológicos.
+    Args:
+    - TWILIO_ACCOUNT_SID (str): SID de la cuenta de Twilio.
+    - TWILIO_AUTH_TOKEN (str): Token de autenticación de Twilio.
+    - input_date (str): Fecha actual en el formato "YYYY-MM-DD".
+    - df (DataFrame): DataFrame con datos meteorológicos.
+    - query (str): Ubicación para la consulta meteorológica.
+    - TO (str): Número de teléfono del destinatario.
+    Returns:
+    - message_id (str): ID del mensaje enviado.
+    """
     account_sid = TWILIO_ACCOUNT_SID
     auth_token = TWILIO_AUTH_TOKEN
     client = Client(account_sid, auth_token)
     message = client.messages \
                     .create(
-                        body='\nHola! \n\n\n El pronostico de lluvia hoy '+ input_date +' en ' + query +' es : \n\n\n ' + str(df),
+                        body='\nHola! \n\n\n El pronóstico de lluvia hoy '+ input_date +' en ' + query +' es : \n\n\n ' + str(df),
                         from_=PHONE_NUMBER,
                         to= TO
                     )
